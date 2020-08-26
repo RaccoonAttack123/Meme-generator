8c6ac737ef80590f965e4c531fffa40cc238f6c7
Ting Liu <mr.tingliu@gmail.com>
2020-08-26 00:05:22 -0700

    updated version

--- meme.js
+++ meme.js
@@ -9,32 +9,43 @@ let t2 = document.getElementById("t2");
 
 //click generate your meme button
 button.addEventListener("click", function(){
-  if(topText.value.length < 20 && bottomText.value.length < 20){
-    document.getElementById("img1").src = image.value;
-    t1.innerHTML = topText.value;
-    t2.innerHTML = bottomText.value;
-    t1.style.color = "white";
-    t2.style.color = "white";
-    t1.style.fontWeight = "bold";
-    t2.style.fontWeight = "bold";
-    t1.style.fontSize = "xx-large";
-    t2.style.fontSize = "xx-large";
-    t1.style.letterSpacing = "8px";
-    t2.style.letterSpacing = "8px";
-    t1.style.textShadow = "0 0 5px #000000, 0 0 5px #000000;";
-    t2.style.textShadow = "0 0 5px #000000, 0 0 5px #000000;";
+  if(topText.value.length < 35 && bottomText.value.length < 35){
+      document.getElementById("img1").src = image.value;
+      t1.innerHTML = topText.value;
+      t2.innerHTML = bottomText.value;
+      t1.style.color = "white";
+      t2.style.color = "white";
+    if(topText.value.length < 20 && bottomText.value.length < 20){
+      t1.style.fontWeight = "bold";
+      t2.style.fontWeight = "bold";
+      t1.style.fontSize = "xx-large";
+      t2.style.fontSize = "xx-large";
+      t1.style.letterSpacing = "8px";
+      t2.style.letterSpacing = "8px";
+      t1.style.textShadow = "0 0 5px #000000, 0 0 5px #000000;";
+      t2.style.textShadow = "0 0 5px #000000, 0 0 5px #000000;";
+    }else if (topText.value.length >= 20 || bottomText.value.length >= 20){
+      t1.style.fontWeight = "bold";
+      t2.style.fontWeight = "bold";
+      t1.style.fontSize = "medium";
+      t2.style.fontSize = "medium";
+      t1.style.letterSpacing = "2px";
+      t2.style.letterSpacing = "2px";
+      t1.style.textShadow = "0 0 5px #000000, 0 0 3px #000000;";
+      t2.style.textShadow = "0 0 5px #000000, 0 0 3px #000000;";  
+    }
     alert('Remove the image by clicking it');
   }
-  if(topText.value.length >= 20 || bottomText.value.length >= 20){
+  if(topText.value.length >= 35 || bottomText.value.length >= 35){
     alert('Text is too long');
     alert('Remove the image by clicking it');
   }
   if(image.value.length === 0){
     document.getElementById("img1").src = "static/add_img.png";
   }
-  topText.value = '';
-  bottomText.value = '';
-  image.value = '';
+    topText.value = '';
+    bottomText.value = '';
+    image.value = '';
 })
 
 //clear picture
