fdcce28555a6172cce730940f1604b50d7d8caa0
Ting Liu <mr.tingliu@gmail.com>
2020-08-26 19:29:12 -0700

    Final version

--- meme.js
+++ meme.js
@@ -9,50 +9,62 @@ let t2 = document.getElementById("t2");
 
 //click generate your meme button
 button.addEventListener("click", function(){
-  if(topText.value.length < 40 && bottomText.value.length < 40){
-      document.getElementById("img1").src = image.value;
-      t1.innerHTML = topText.value;
-      t2.innerHTML = bottomText.value;
-      t1.style.color = "white";
-      t2.style.color = "white";
-    if(topText.value.length < 20 && bottomText.value.length < 20){
-      t1.style.fontWeight = "bold";
-      t2.style.fontWeight = "bold";
-      t1.style.fontSize = "xx-large";
-      t2.style.fontSize = "xx-large";
-      t1.style.letterSpacing = "8px";
-      t2.style.letterSpacing = "8px";
-      t1.style.textShadow = "0 0 5px #000000, 0 0 5px #000000;";
-      t2.style.textShadow = "0 0 5px #000000, 0 0 5px #000000;";
-    }else if (topText.value.length >= 20 || bottomText.value.length >= 20){
-      t1.style.fontWeight = "bold";
-      t2.style.fontWeight = "bold";
-      t1.style.fontSize = "x-large";
-      t2.style.fontSize = "x-large";
-      t1.style.letterSpacing = "2px";
-      t2.style.letterSpacing = "2px";
-      t1.style.textShadow = "0 0 5px #000000, 0 0 3px #000000;";
-      t2.style.textShadow = "0 0 5px #000000, 0 0 3px #000000;";  
-    }
-    alert('Remove the image by clicking it');
-  }
-  if(topText.value.length >= 40 || bottomText.value.length >= 40){
-    alert('Text is too long');
-    alert('Remove the image by clicking it');
-  }
-  if(image.value.length === 0){
-    document.getElementById("img1").src = "static/add_img.png";
-  }
+
+  const newMeme = document.createElement('div');
+  newMeme.className = 'container';
+  newMeme.id = 'base-image';
+  const imageElem = document.createElement('img'); 
+  imageElem.className = 'image';
+  imageElem.id = 'img1';
+  imageElem.src = image.value;
+
+  
+  const newTextImageContainer = document.createElement('div');
+  newTextImageContainer.className = 'text_image';
+  
+  const firstText = document.createElement('p');
+  firstText.id ='t1';
+  firstText.innerHTML = topText.value;
+  
+  const secondText = document.createElement('p');
+  secondText.id ='t1';
+  secondText.innerHTML = bottomText.value;
+  
+  const break1 = document.createElement('br');
+  const break2 = document.createElement('br');
+  const break3 = document.createElement('br');
+  const break4 = document.createElement('br');
+   
+  const xButton = document.createElement('button')
+  xButton.innerHTML = 'X'
+  xButton.id= 'x-button';
+  xButton.addEventListener('click', () => document.body.removeChild(newMeme));
+
+  document.body.appendChild(newMeme);
+  newMeme.appendChild(imageElem);
+  newMeme.appendChild(newTextImageContainer);
+  newTextImageContainer.appendChild(firstText);
+  newTextImageContainer.appendChild(break1);
+  newTextImageContainer.appendChild(break2);
+  newTextImageContainer.appendChild(break3);
+  newTextImageContainer.appendChild(break4);
+  newTextImageContainer.appendChild(secondText);
+  newMeme.appendChild(xButton);
+
     topText.value = '';
     bottomText.value = '';
     image.value = '';
+
+    alert("Scroll down to see added images; click 'X' to delete image");
 })
 
+
+
 //clear picture
-image1.addEventListener("click", function(){
-  image1.src = "static/add_img.png";
-  document.getElementById("t1").innerHTML = "";
-  document.getElementById("t2").innerHTML = "";
-  alert('Image has been removed');
-})
+// image1.addEventListener("click", function(){
+//   image1.src = "static/add_img.png";
+//   document.getElementById("t1").innerHTML = "";
+//   document.getElementById("t2").innerHTML = "";
+//   alert('Image has been removed');
+// })
 
